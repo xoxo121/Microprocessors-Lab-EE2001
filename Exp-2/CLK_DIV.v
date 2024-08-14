bd106792476ad5dd87f4d781533f8c40d4c5ccef
NikhilSwaroop-P <ee23b055@smail.iitm.ac.in>
2024-08-14 16:32:44 +0530

    Update CLK_DIV.v

--- Exp-2/CLK_DIV.v
+++ Exp-2/CLK_DIV.v
@@ -4,27 +4,27 @@ module clk_divider(
     output reg outClk
     );
 
-reg [3:0] clockCount;
+reg [25:0] clockCount;
  // Adjust the value of v to change the frequency of outClk
-parameter v = 4'd10;
+parameter v = 25'd50000000;
 
 always @(negedge reset or posedge inClk) 
 begin
     if (reset == 1'b0) 
     begin
-        clockCount <= 4'b0000;
+        clockCount <= 25'd0;
         outClk <= 1'b0;
     end 
     else 
     begin
         // Increment clockCount
-        clockCount <= clockCount + 1'b1;
+        clockCount <= clockCount + 1'd1;
 
         // Check if clockCount has reached v
-        if (clockCount == v-1) 
+        if (clockCount == v) 
         begin
             // Reset clockCount and toggle outClk
-            clockCount <= 4'b0000;
+            clockCount <= 25'd0;
             outClk <= ~outClk;
         end
     end
