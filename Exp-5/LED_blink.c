bbd4048c0eb9657bdad7a31398756ff74c1bcd5b
Rahul Vimalkanth <143904650+xoxo121@users.noreply.github.com>
2024-09-11 01:17:04 +0530

    Create LED_blink.c

---
+++ Exp-5/LED_blink.c
@@ -0,0 +1,14 @@
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void)
+{
+    DDRD = 0x03;
+    while (1) 
+    {
+		PORTD = 0x01;
+		_delay_ms(1000);
+		PORTD = 0x02;
+		_delay_ms(1000);
+    }
+}
